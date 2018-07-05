614d12497f6c14014fd574f3279f2ae79923b7f4
userpro <hottterest@gmail.com>
2018-07-05 21:54:53 +0800

    .

--- model/chat_db.js
+++ model/chat_db.js
@@ -1,2 +1,16 @@
-var redis = require('redis');
-var client = redis.createClient();
+const redis = require('redis');
+const client = redis.createClient();
+
+const chat_key = 'chat_room';
+
+class ChatDB
+{
+	constructor() {}
+	add(data) { return client.lpush(chat_key, data); }
+	get(start, n) { 
+		return client.llen(chat_key, function(err, res){
+			if (err) throw err;
+			else client.lrange(chat_key, start, (start+n)>res?-1:(start+n));
+		}); 
+	}
+}
